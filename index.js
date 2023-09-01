f0a0b4c4f94f1dc02b22a447cf8ec7bd69067a6d
Nicholas Serrano <ns102894@harriscomputer.com>
2023-08-31 18:39:45 -0600

    making a few more cors changes

--- index.js
+++ index.js
@@ -24,12 +24,16 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
+//app.use(helmet());
+//app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+//app.use(morgan("common"));
+
+//app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.json());
+
+//app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+//app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 app.use((req, res, next) => {
